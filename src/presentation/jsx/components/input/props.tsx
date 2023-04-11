9e8c5fbeefee4b8c854f72f4f0a344ca776f2c98
diegoalvescf <dalvesdocs@gmail.com>
2023-04-10 21:19:17 -0300

    adjust input to add reference

--- src/presentation/jsx/components/input/props.tsx
+++ src/presentation/jsx/components/input/props.tsx
@@ -1,3 +1,6 @@
-import { TextInputProps } from 'react-native';
+import React from 'react';
+import { TextInput, TextInputProps } from 'react-native';
 
-export interface InputComponentProps extends TextInputProps {}
+export interface InputComponentProps extends TextInputProps {
+  inputRef?: React.RefObject<TextInput>;
+}
